53680f5ebc5c39f4266cc81eab4acc86d904e8c8
Heather Bearden-Tellez <bearden-tellez@Heathers-MacBook-Pro.local>
2016-08-18 11:06:29 -0400

    try it

---
+++ src/tiyinc/noobs/Day9Runner.java
@@ -0,0 +1,10 @@
+package tiyinc.noobs;
+
+/**
+ * Created by bearden-tellez on 8/18/16.
+ */
+public class Day9Runner {
+    public static void main(String[] args){
+        System.out.println("Running...");
+    }
+}
