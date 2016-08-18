58c18a264d00b63a97ec616765cf40f1c74e81fd
Heather Bearden-Tellez <bearden-tellez@Heathers-MacBook-Pro.local>
2016-08-18 11:13:07 -0400

    with add

--- src/tiyinc/noobs/Day9Runner.java
+++ src/tiyinc/noobs/Day9Runner.java
@@ -1,10 +1,23 @@
 package tiyinc.noobs;
 
+import java.io.File;
+
 /**
  * Created by bearden-tellez on 8/18/16.
  */
 public class Day9Runner {
     public static void main(String[] args){
         System.out.println("Running...");
+        Day9Runner  myRunner = new Day9Runner();
+        myRunner.testWriteFile(); //not static so it can't be instatiated
     }
+    public void testWriteFile() {
+        System.out.println(" testWriteFile()");
+        try {
+            File testFile = new File("noobs.txt");
+        }catch (Exception exception) {
+            exception.printStackTrace();
+        }
+    }
+
 }
